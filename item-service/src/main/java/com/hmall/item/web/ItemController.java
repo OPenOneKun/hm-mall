78647c86fa6da28215be09b618e7f30880527cb0
openone <838206860@qq.com>
2022-04-17 11:26:03 +0800

    添加网关模块，说明文档

--- item-service/src/main/java/com/hmall/item/web/ItemController.java
+++ item-service/src/main/java/com/hmall/item/web/ItemController.java
@@ -2,6 +2,7 @@ package com.hmall.item.web;
 
 import com.hmall.item.service.IItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,7 @@ public class ItemController {
 
     @Autowired
     private IItemService itemService;
+
+    @GetMapping
+    public
 }
